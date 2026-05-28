c866d333f568b174ae13cac4ae573eed16e838ad
Damcza Dev <drma801@gmail.com>
2026-05-28 20:19:49 +0700

    Enable GLSL imports in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), glsl()],
   resolve: {
     alias: {
       "lucide-react": "/src/lib/lucide-react.tsx",
