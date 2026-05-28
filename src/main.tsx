f33276d8fb55a19edf5600317328b9e15c8bfb01
Damcza Dev <drma801@gmail.com>
2026-05-28 23:25:24 +0700

    Apply editorial reset and disable welcome autoplay

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { installAudioPolicy } from "./lib/audio-policy";
 import "./styles.css";
 import "./effects.css";
+import "./editorial-reset.css";
+
+installAudioPolicy();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
